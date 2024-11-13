1c6904706214181da109c9f10fb5fba6be92b32d
agchdev <alex.chinchi02@gmail.com>
2024-11-13 12:22:31 +0100

    cambios

--- js/app.js
+++ js/app.js
@@ -1,25 +1,28 @@
 // ELEMENTOS DINÁMICOS DE DOM
-const appDivIcon = document.createElement("div");
 appIconImg = ["img/ico/recycle_bin_full.ico","img/ico/directory_open.ico", "img/ico/msie2.ico"];
 appIconTxt = ["Papelera", "Documentos", "InternetShop"];
 // FUNCIONES
 function crearAppIcon(){ // Esta función creal la app del escritorio
     cont = 0;
-    appIcon.forEach(link => {
+    appIconImg.forEach(link => {
+        const appDivIcon = document.createElement("div");
+        appDivIcon.setAttribute("class", "pointer iconDesktop")
         const img = document.createElement("img");
         img.setAttribute("class", "pointer");
         img.src = link;
         const btn = document.createElement("button");
         btn.setAttribute("class", "pointer");
-        btn.textContent = appIconTxt[0];
+        btn.textContent = appIconTxt[cont];
         cont++;
+        console.log(cont);
+        appDivIcon.append(img, btn);
+        document.querySelector("#aps").append(appDivIcon);
     });
-
-    appDivIcon.append(img, btn);
+    
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     // ELEMENTOS EXISTENTES EN EL DOM
     const sectionApp = document.querySelector("#aps");
-
+    crearAppIcon();
 })
